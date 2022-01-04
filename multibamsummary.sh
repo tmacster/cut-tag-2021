364fee8f838e70fe36f09683381090f0d23f5985
tmacster <tmacster@users.noreply.github.com>
2022-01-03 16:20:14 -0800

    Update multibamsummary.sh

--- multibamsummary.sh
+++ multibamsummary.sh
@@ -3,13 +3,13 @@
 hist1="k27"
 hist2="k4"
 
-# loop through files to create correlation (pearson)
+# loop through files to create correlation
 
-multiBamSummary bins --bamfiles ~/cnt_2021/sorted_bam/S[1-8]_sorted.bam -o ${hist1}_bamsummary.npz -p max
-multiBamSummary bins --bamfiles ~/cnt_2021/sorted_bam/S[9-16]_sorted.bam -o ${hist2}_bamsummary.npz -p max
+multiBamSummary bins --bamfiles ~/cnt_2021/sorted_bam/${hist1}/S[1-8]_sorted.bam -o ${hist1}_bamsummary.npz -p max
+multiBamSummary bins --bamfiles ~/cnt_2021/sorted_bam/${hist2}/*_sorted.bam -o ${hist2}_bamsummary.npz -p max
 
 
-# now plot, k27 vs k4 as separate files
+# now plot, with k27 vs k4 as separate files
 
 for i in ${hist1} ${hist2}; do
 	echo "plotting correlation $i"
@@ -17,8 +17,14 @@ for i in ${hist1} ${hist2}; do
 
 	plotCorrelation -in "$i"_bamsummary.npz --corMethod pearson \
 	 --skipZeros --plotTitle "${i} bam correlation" \
-	 --whatToPlot heatmap --colorMap RdYlBu --plotNumbers \
+	 --whatToPlot heatmap --plotFileFormat pdf \
+	 --colorMap RdYlBu --plotNumbers \
 	 -o "$i"_bam_pearson.pdf \
-	 --outFileMatrix "$i"_bam_pearson_counts.txt
+	 --outFileCorMatrix "$i"_bam_pearson_counts.txt
+
+	 plotPCA -in "$i"_bamsummary.npz --transpose \
+	 --plotFileFormat pdf \
+	 -o "$i"_bam_pearson_PCA.pdf \
+	 --plotTitle "${i} PCA of read counts" 
 
 done

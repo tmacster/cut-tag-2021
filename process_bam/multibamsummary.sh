fda848b0f28cc533057a07f1aa9a042f37a6bd27
tmacster <tmacster@users.noreply.github.com>
2022-03-30 11:46:15 -0700

    Update and rename multibamsummary.sh to process_bam/multibamsummary.sh

--- process_bam/multibamsummary.sh
+++ process_bam/multibamsummary.sh
@@ -3,20 +3,21 @@
 hist1="k27"
 hist2="k4"
 
-# loop through files to create correlation
+# loop through files to correlate reps and plot
+# pairwise correlation (Spearman)
 
-multiBamSummary bins --bamfiles ~/cnt_2021/sorted_bam/${hist1}/S[1-8]_sorted.bam -o ${hist1}_bamsummary.npz -p max
-multiBamSummary bins --bamfiles ~/cnt_2021/sorted_bam/${hist2}/*_sorted.bam -o ${hist2}_bamsummary.npz -p max
+multiBamSummary bins --bamfiles ~/cnt_2021/sorted_bam/"$hist1"/S[1-8]_sorted.bam -o "$hist1"_bamsummary.npz -p max
+multiBamSummary bins --bamfiles ~/cnt_2021/sorted_bam/"$hist2"/*_sorted.bam -o "$hist2"_bamsummary.npz -p max
 
 
 # now plot, with k27 vs k4 as separate files
 
-for i in ${hist1} ${hist2}; do
+for i in "$hist1" "$hist2"; do
 	echo "plotting correlation $i"
 	echo ""
 
 	plotCorrelation -in "$i"_bamsummary.npz --corMethod spearman \
-	 --skipZeros --plotTitle "${i} bam correlation" \
+	 --skipZeros --plotTitle "$i bam correlation" \
 	 --whatToPlot heatmap --plotFileFormat pdf \
 	 --colorMap RdYlBu --plotNumbers \
 	 -o "$i"_bam_spearman.pdf \
@@ -25,7 +26,7 @@ for i in ${hist1} ${hist2}; do
 	 plotPCA -in "$i"_bamsummary.npz --transpose \
 	 --plotFileFormat pdf \
 	 -o "$i"_bam_PCA.pdf \
-	 --plotTitle "${i} PCA of read counts" 
+	 --plotTitle "$i PCA of read counts" 
 
 done
 
